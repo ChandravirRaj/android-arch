44693f8ee83a04e3782a7e845c91047756ffc263
chandravir <chandravir.singh@mobcoder.com>
2023-07-19 00:27:29 +0530

    create dev branch

---
+++ app/src/main/java/com/androboy/androidarch/ArchApp.kt
@@ -0,0 +1,10 @@
+package com.androboy.androidarch
+
+import android.app.Application
+
+class ArchApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- app/src/main/java/com/androboy/androidarch/MainActivity.kt
+++ app/src/main/java/com/androboy/androidarch/MainActivity.kt
@@ -2,7 +2,9 @@ package com.androboy.androidarch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+/**
+ * This is main activity
+ * */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
