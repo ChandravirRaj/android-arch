22e670ba01970e70e757f713e3a91f3e45639032
chandravir <chandravir.singh@mobcoder.com>
2023-07-19 14:29:51 +0530

    add dependencies for lifecycle,coroutines

--- app/src/main/java/com/androboy/androidarch/ArchApp.kt
+++ app/src/main/java/com/androboy/androidarch/ArchApp.kt
@@ -1,7 +1,9 @@
 package com.androboy.androidarch
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class ArchApp: Application() {
 
     override fun onCreate() {
